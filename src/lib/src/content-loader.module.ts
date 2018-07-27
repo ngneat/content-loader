a62c71cc828debeb8ffcfaf6c0657b128c3ac858
Daniel <mytechde@outlook.com>
2018-07-27 10:45:48 +0200

    refactor(lib): remove unused imports

--- src/lib/src/content-loader.module.ts
+++ src/lib/src/content-loader.module.ts
@@ -1,19 +1,18 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+
+import { BulletListContentLoaderComponent } from './bullet-list-loader.component';
 import { ContentLoaderComponent } from './content-loader.component';
 import { FacebookContentLoaderComponent } from './facebook-loader.component';
-import { BulletListContentLoaderComponent } from './bullet-list-loader.component';
 import { ListContentLoaderComponent } from './list-loader.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   declarations: [
     ContentLoaderComponent,
     FacebookContentLoaderComponent,
     BulletListContentLoaderComponent,
-    ListContentLoaderComponent,
+    ListContentLoaderComponent
   ],
   exports: [
     ContentLoaderComponent,
