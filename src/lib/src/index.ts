64280f62677d97d5b7514081cc8fc4058de2afa3
Netanel Basal <netanel7799@gmail.com>
2018-10-20 20:20:25 +0300

    clean

--- src/lib/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './content-loader.component';
-export * from './content-loader.module';
-export * from './facebook-loader.component';
-export * from './list-loader.component';
-export * from './bullet-list-loader.component';
